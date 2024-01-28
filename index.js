10c2a2acb1193ada6e4a23b5be3e8344fa130e67
DasTapan <tapands.das@gmail.com>
2024-01-28 09:57:59 +0530

    Finish

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("node:http");
+const fsp = require("node:fs/promises");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  const pathname = req.url;
+  const fileName = `.${pathname === "/" ? "/index" : pathname}.html`;
+
+  const writeContent = async () => {
+    try {
+      const data = await fsp.readFile(fileName, "utf8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      return res.end(data);
+    } catch (error) {
+      const notFound = await fsp.readFile("./404.html", "utf8");
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end(notFound);
+    }
+  };
+
+  writeContent();
+});
+
+server.listen(8080, () => console.log("listening request at port 8080.."));
